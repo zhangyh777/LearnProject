96037d891158ffb849a7ac2bcec078e381548390
zyh <205099027@qq.com>
2022-07-07 10:31:24 +0800

    test3 add

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
 
     }
 }
