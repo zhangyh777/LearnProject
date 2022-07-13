c4cdd75610a540a73531954406a2c33db0fa043b
zyh <205099027@qq.com>
2022-07-13 22:18:18 +0800

    druid

---
+++ src/connectPool/DruidTest.java
@@ -0,0 +1,34 @@
+package connectPool;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * 德鲁伊连接池（阿里开发,性能最好）
+ */
+public class DruidTest {
+    @Test
+    public void druidTest() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src\\druid.properties"));
+
+        //创建Druid连接池
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 500000; i++) {
+            Connection connection = dataSource.getConnection();
+            connection.close();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Druid连接池建立500000次连接耗时"+(end-start)+"ms");
+
+    }
+}
